771e71b959298a6945a2d30e3c9030ed5aa299e0
cmhd <cmhd@users.noreply.github.com>
2015-01-02 04:10:09 -0500

    initialize

---
+++ src/main/scala/cmhd/mods/adapt/AdaptMod.scala
@@ -0,0 +1,10 @@
+package cmhd.mods.adapt
+
+import cpw.mods.fml.common.Mod
+
+@Mod(modid = AdaptMod.ModId, name = AdaptMod.ModName, version = AdaptMod.ModVersion, modLanguage = "scala")
+object AdaptMod {
+  final val ModId = "adapt"
+  final val ModName = "Adapt"
+  final val ModVersion = "0.0.0"
+}
